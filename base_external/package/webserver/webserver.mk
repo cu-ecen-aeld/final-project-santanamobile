ce58151346256313fa8b7e8d923f73eb4d2a66fc
Helder Santana <3494526+santanamobile@users.noreply.github.com>
2024-12-07 08:53:09 -0300

    Buildroot: changes on wwwroot

--- base_external/package/webserver/webserver.mk
+++ base_external/package/webserver/webserver.mk
@@ -12,7 +12,7 @@ WEBSERVER_LICENSE = MIT
 
 define WEBSERVER_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/app/ $(TARGET_DIR)/var/www/server/
-	$(INSTALL) -m 0755 $(@D)/app/* $(TARGET_DIR)/var/www/server/
+	$(INSTALL) -m 0755 $(@D)/app/app.py $(TARGET_DIR)/var/www/server/app.py
 endef
 
 $(eval $(generic-package))
---
+++ base_external/rootfs_overlay/var/www/server/config.py
@@ -0,0 +1,5 @@
+USERNAME   = 'beagle'
+PASSWORD   = 'bonepass'
+MOUNT_POINT= '/media/usb0'
+NETWORK_INTERFACE = 'eth0'
+
