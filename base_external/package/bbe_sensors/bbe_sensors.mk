de723a4ff0741df6e79359c4ad4597f632133a1f
Helder Santana <3494526+santanamobile@users.noreply.github.com>
2024-12-11 21:15:17 -0300

    Buildroot: Add package "BBE Sensors" from my fork of the Sancloud repository.

---
+++ base_external/package/bbe_sensors/bbe_sensors.mk
@@ -0,0 +1,21 @@
+##############################################################
+#
+# BBE_SENSORS
+#
+##############################################################
+BBE_SENSORS_VERSION = 'e63470f339e27de405235f2c7caa35b7d6ac12f6'
+BBE_SENSORS_SITE = 'git@github.com:santanamobile/BBE_Sensors.git'
+BBE_SENSORS_SITE_METHOD = git
+BBE_SENSORS_GIT_SUBMODULES = YES
+
+define AESD_ASSIGNMENTS_BUILD_CMDS
+        $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/sc_mpu6050 all
+        $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/sc_st_pressure all
+endef
+
+define BBE_SENSORS_INSTALL_TARGET_CMDS
+    $(INSTALL) -d 0755 $(@D)/sc_mpu6050/mpu_reader $(TARGET_DIR)/bin
+    $(INSTALL) -d 0755 $(@D)/sc_st_pressure/pressure_reader $(TARGET_DIR)/bin
+endef
+
+$(eval $(generic-package))
