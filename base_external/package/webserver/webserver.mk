c932729eceaf46f2df9b319be64b78145666ce4f
Helder Santana <3494526+santanamobile@users.noreply.github.com>
2024-12-13 06:19:52 -0300

    Buildroot: Fix coding style of packages to follow the guidelines on the buildroot manual.

--- base_external/package/webserver/webserver.mk
+++ base_external/package/webserver/webserver.mk
@@ -1,8 +1,9 @@
-##############################################################
+################################################################################
 #
-# WEBSERVER
+# webserver
 #
-##############################################################
+################################################################################
+
 WEBSERVER_VERSION = '98c1e1fd9404c315f8d3e101f1a95cef99e5939b'
 WEBSERVER_SITE = 'git@github.com:santanamobile/beaglenas.git'
 WEBSERVER_SITE_METHOD = git
@@ -13,6 +14,7 @@ WEBSERVER_LICENSE = MIT
 define WEBSERVER_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/app/ $(TARGET_DIR)/var/www/server/
 	$(INSTALL) -m 0755 $(@D)/app/app.py $(TARGET_DIR)/var/www/server/app.py
+	$(INSTALL) -m 0755 $(@D)/app/config-sample.py $(TARGET_DIR)/var/www/server/config.py
 endef
 
 $(eval $(generic-package))
