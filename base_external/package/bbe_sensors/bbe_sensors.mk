aa835891b6855fa94d42648ba0cf6971c22fdb93
Helder Santana <3494526+santanamobile@users.noreply.github.com>
2024-12-17 14:09:10 -0300

    Fix package: bbe_sensors

--- base_external/package/bbe_sensors/bbe_sensors.mk
+++ base_external/package/bbe_sensors/bbe_sensors.mk
@@ -9,9 +9,9 @@ BBE_SENSORS_SITE = 'git@github.com:santanamobile/BBE_Sensors.git'
 BBE_SENSORS_SITE_METHOD = git
 BBE_SENSORS_GIT_SUBMODULES = YES
 
-define AESD_ASSIGNMENTS_BUILD_CMDS
-        $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/sc_mpu6050 all
-        $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/sc_st_pressure all
+define BBE_SENSORS_BUILD_CMDS
+    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/sc_mpu6050 all
+    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/sc_st_pressure all
 endef
 
 define BBE_SENSORS_INSTALL_TARGET_CMDS
